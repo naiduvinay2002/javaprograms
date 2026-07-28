19de88db51f9b64ebb65d67521e9118ac86a2e12
naiduvinay2002 <naiduvinay21@gmail.com>
2026-07-28 21:12:04 +0530

    app

---
+++ src/com/app/App.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import com.calculator.Calculator;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Calculator c= new Calculator();
+		c.add();
+		c.sub();
+		c.mul();
+		c.div();
+		
+	}
+
+}
